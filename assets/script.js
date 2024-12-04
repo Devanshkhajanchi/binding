d172c00139109e5e89de3637023aa004704f21a2
Devansh Khajanchi <khajd104@newschool.edu>
2024-12-04 11:26:23 -0500

    Styled random article on homepage

--- assets/script.js
+++ assets/script.js
@@ -12,14 +12,14 @@ if(articleNum==1){
 	articleLink.href = "call-for-criticism/index.html"
 	titleBox.innerHTML = 'Call for Criticism'
 	articleAuthor.innerHTML = 'Massimo Vignelli, 1983'
-	initials.innerHTML = 'cc'
+	initials.innerHTML = 'CC'
 	articleBackground.style.backgroundColor = "#FB01F1"
 } 
 else if (articleNum==2){
 	titleBox.innerHTML = 'Graphic Design Criticism as a Spectator Sport.'
 	articleLink.href = "graphic-design-criticism-as-a-spectator-sport/index.html"
 	articleAuthor.innerHTML = 'Michael Bierut, 2013'
-	initials.innerHTML = 'ss'
+	initials.innerHTML = 'SS'
 	articleBackground.style.backgroundColor = "#EDE716"
 }
 else {
@@ -27,7 +27,7 @@ else {
 
 	articleLink.href = "youll-never-guess-the-amazing-ways-online-design-criticism-and-writing-has-changed/index.html"
 	articleAuthor.innerHTML = 'Chappell Ellison, 2014'
-	initials.innerHTML = 'dw'
+	initials.innerHTML = 'DC'
 	articleBackground.style.backgroundColor = "#6583FC"
 }
 
